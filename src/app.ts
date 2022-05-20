42b353fd911a76a7a59b682de55a1fc4d4ce57f0
AndreyGalchevski <andreygalchevski@gmail.com>
2022-05-20 21:30:15 +0300

    use dotenv and prepare for deploy to Heroku

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 
@@ -5,7 +7,7 @@ import gameRoutes from "./routes/game-routes";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json({ limit: "50kb" }));
 
 app.use("/game", gameRoutes);
